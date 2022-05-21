df7b3259fae817ab9c74eae9cccda94b7f8c7afc
Rafael León <41965141+rafa2701@users.noreply.github.com>
2022-05-20 23:16:20 -0400

    2nd commit

--- web_boda/get_link.php
+++ web_boda/get_link.php
@@ -2,8 +2,9 @@
 // get database Connection and check if there is a valid link
 $link=null;
 require_once('MySQLi.class.php');
-$db = new MySQL_Wrapper("boda.epizy.com", "link", "pass", "db_name");
-$link  = $db->getOneRow("SELECT * FROM link LIMIT 1");
+$db = new MySQL_Wrapper("sql210.epizy.com", "epiz_29161641", "WM3kpvvNdvYxY", "epiz_29161641_boda");
+$data  = $db->getOneRow("SELECT * FROM boda LIMIT 1");
+$link=$data["link"];
 
 //Redirect to the valid link or to the waiting page
 if(isset($link)){
